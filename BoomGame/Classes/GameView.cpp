2fc1c1e242951f2781d5f0a1d35e4f82e5ac5923
qz515880 <1195933889@qq.com>
2015-04-09 17:28:44 +0800

    添加瞄准的辅助线的计算公式

--- BoomGame/Classes/GameView.cpp
+++ BoomGame/Classes/GameView.cpp
@@ -1,6 +1,6 @@
 #include "GameView.h"
 #define GRAVITR (10)
-#define MAX_SPEED (25)//抛出物体的最大速度
+#define MAX_SPEED (75)//抛出物体的最大速度
 
 
 bool GameView::init(){
@@ -11,27 +11,27 @@ bool GameView::init(){
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
 	//boom test 
-    curBoom = createBoom(1);
+	curBoom = createBoom(1);
 	curBoom->setAnchorPoint(Point::ANCHOR_MIDDLE);
 	curBoom->setPosition(visibleSize.width / 5, visibleSize.height / 4);
 	this->addChild(curBoom);
 	//add touch event
 	auto touchEvent = EventListenerTouchOneByOne::create();
-	touchEvent->onTouchBegan = CC_CALLBACK_2(GameView::onTouchBegan,this);
-	touchEvent->onTouchMoved = CC_CALLBACK_2(GameView::onTouchMoved,this);
+	touchEvent->onTouchBegan = CC_CALLBACK_2(GameView::onTouchBegan, this);
+	touchEvent->onTouchMoved = CC_CALLBACK_2(GameView::onTouchMoved, this);
 	touchEvent->onTouchEnded = CC_CALLBACK_2(GameView::onTouchEnded, this);
 	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchEvent, this);
 	//scheduleUpdate();
-	schedule(schedule_selector(GameView::updateTest),1.0f, kRepeatForever,0);
+	schedule(schedule_selector(GameView::updateTest), 1.0f, kRepeatForever, 0);
 	return true;
 }
 
 void GameView::initData(){
-	 degree = 0;
-	 speed = 0;
-	 //speed_x = 0;
-	 //speed_y = 0;
-	 shoting = false;
+	degree = 0;
+	speed = 0;
+	//speed_x = 0;
+	//speed_y = 0;
+	shoting = false;
 }
 
 Boom* GameView::createBoom(int id){
@@ -48,8 +48,10 @@ bool GameView::onTouchBegan(Touch *touch, Event  *event){
 
 void GameView::onTouchMoved(Touch *touch, Event  *event){
 	Point curpoint = touch->getLocation();
+	degree = getDegreeByPoint(touchlocation, curpoint);
 	int distance = curpoint.distance(touchlocation);
-
+	speed = getSpeedByDistance(distance);
+	/*calculateSubLine(curBoom->getPosition(), speed, degree);*/
 }
 
 void GameView::onTouchEnded(Touch *touch, Event  *event){
@@ -58,10 +60,11 @@ void GameView::onTouchEnded(Touch *touch, Event  *event){
 	degree = getDegreeByPoint(touchlocation, endpint);
 	//get the force by distance
 	speed = getSpeedByDistance(distance);
+	calculateSubLine(curBoom->getPosition(), speed, degree);
 	shoting = true;
 }
 
-float GameView::getDegreeByPoint(Point sp,Point ep){
+float GameView::getDegreeByPoint(Point sp, Point ep){
 	float deg = atan(abs(ep.y - sp.y) / abs(ep.x - sp.x));
 	CCLOG("DEGREE = %f", CC_RADIANS_TO_DEGREES(deg));
 	//decide the direction
@@ -71,7 +74,7 @@ float GameView::getDegreeByPoint(Point sp,Point ep){
 //get force by move distacne
 float GameView::getSpeedByDistance(float distance){
 	if (distance > 0){
-		float mySpeed = distance * 1;//
+		float mySpeed = distance * 100;//
 		if (mySpeed > MAX_SPEED){
 			mySpeed = MAX_SPEED;
 		}
@@ -84,28 +87,53 @@ void GameView::update(float dt){
 	if (shoting){
 		/*speed_y += GRAVITR*dt;
 		curBoom->setPosition(curBoom->getPosition().x + speed_x, curBoom->getPosition().y - speed_y);*/
-	}	
+	}
 }
 
 void GameView::updateTest(float dt){
 	if (shoting){
 		//speed_y += GRAVITR*dt;
 		//curBoom->setPosition(curBoom->getPosition().x + speed_x, curBoom->getPosition().y - speed_y);
+		if (subline.size() > 0){
+			for (Point dp : subline){
+				DrawNode* drawNode = DrawNode::create();
+				this->addChild(drawNode);
+				drawNode->drawDot(dp, 5, Color4F(255, 255, 255, 255));
+			}
+		}
 	}
 }
 
-std::vector <Point> GameView::calculateSubLine(Point sp,float speed, float degree){
+std::vector <Point> GameView::calculateSubLine(Point sp, float speed, float degree){
+	CCLOG("degree = %f", CC_RADIANS_TO_DEGREES(degree));
+	CCLOG("start.x =%f", sp.x);
+	CCLOG("start.y =%f", sp.y);
 	float speed_x = speed*cos(degree);
 	float speed_y = speed*sin(degree);
+	CCLOG("speed.x = %f", speed_x);
+	CCLOG("speed.y = %f", speed_y);
 	float t = speed_y / GRAVITR;
-	Point top = Point(sp.x+speed_x*t, sp.y+speed_y - GRAVITR*t*t / 2);//抛物线的顶点坐标
+	CCLOG("move time =%f", t);
+	Point tp = Point(sp.x + speed_x*t, sp.y + speed_y*t - GRAVITR*t*t / 2);//抛物线的顶点坐标
+	CCLOG("TOP.x = %f", tp.x);
+	CCLOG("TOP.y = %f", tp.y);
+	Point ep = Point(sp.x + speed_x*t * 2, sp.y);
+	//拉格朗日插值法
+	float p = sp.y / ((sp.x - tp.x)*(sp.x - ep.x));
+	float q = tp.y / ((tp.x - sp.x)*(tp.x - ep.x));
+	float r = ep.y / ((ep.x - sp.x)*(ep.x - tp.x));
+	float A = p + q + r;
+	float B = -p*(tp.x + ep.x) - q*(sp.x + ep.x) - r*(sp.x + tp.x);
+	float C = p*tp.x*ep.x + q*sp.x*ep.x + r*sp.x*tp.x;
 	//间隔5个像素，绘制一个点
 	float y = sp.y;
 	float x = sp.x;
 	while (y > 0){
 		x += 5;
-		y = -(x - top.x)*(x - top.x) + top.y;
-		subline.push_back(Point(x,y));
+		y = A*x*x + B*x + C;
+		subline.push_back(Point(x, y));
+		CCLOG("subline x = %f", x);
+		CCLOG("subline y = %f", y);
 	}
 	return subline;
-} 
+}
